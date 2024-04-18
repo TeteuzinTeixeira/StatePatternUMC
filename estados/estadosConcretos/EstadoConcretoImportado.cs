74877a379a18feaaf84d4d3f1668641897705192
iago silva <iago.developer01@gmail.com>
2024-04-18 11:12:27 -0300

    conclusão do fluxo de chamadas dos States

--- estados/estadosConcretos/EstadoConcretoImportado.cs
+++ estados/estadosConcretos/EstadoConcretoImportado.cs
@@ -10,8 +10,11 @@ class EstadoConcretoImportado : Estado
     public override void Handle2()
     {
         Console.WriteLine("EstadoConcretoImportado capta a segunda requisição");
+        // Transição para um novo estado aqui
+        Console.WriteLine("EstadoConcretoImportado deseja mudar o estado do contexto");
+        this._context.TransitionTo(new EstadoConcretoManual());  // Insira a transição desejada
     }
-    
+
     public override void Handle3()
     {
         Console.WriteLine("EstadoConcretoImportado capta a terceira requisição");
